3316a4fc731e0fdc8ab75516f2d32c85a14d1e4a
MK <m.krzesniak@gmail.com>
2025-02-09 19:33:31 +0100

    Pierwszy program Hello. Tablica args.

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = args[0];// pobiera z konsoli pierwszy element tablicy, tam wpisałem Marcin
+            Console.WriteLine("Witaj, " + name);
+
+            //foreach (var item in args)
+            //{
+            //    Console.WriteLine("Witaj " + item);
+            //}
+
+        }
+    }
+} 
